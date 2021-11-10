d2b5e980cdb07be0c440a023e211fe825bad6ea0
kamilzan <a.kamilzan@gmail.com>
2021-11-10 17:27:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+    /*jshint esversion: 6 */
+(function () {
+    'use strict';
+    let number =5;
+    number=10;
+    console.log(number);
+    let coins=1;
+    const answer = prompt("How are you?");
+    console.log(answer);
+}());
